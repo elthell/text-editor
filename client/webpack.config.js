67804f87859d1a3afac51751e47e98fd375efae8
elthell <eleanor.thell@gmail.com>
2023-10-26 19:34:59 -0500

    test run start + update webpack names

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -21,7 +21,7 @@ module.exports = () => {
       // Webpack plugin that generates our html file and injects our bundles.
       new HtmlWebpackPlugin({
         template: "./index.html",
-        title: "Contact Cards",
+        title: "J.A.T.E",
       }),
 
       // Injects our custom service worker
@@ -34,9 +34,9 @@ module.exports = () => {
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
-        name: "Contact Cards",
-        short_name: "Contact",
-        description: "Never forget your contacts!",
+        name: "J.A.T.E",
+        short_name: "JATE",
+        description: "Just another text editor",
         background_color: "#225ca3",
         theme_color: "#225ca3",
         start_url: "./",
